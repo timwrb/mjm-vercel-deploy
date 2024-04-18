c4c18932c1fbcc44fca3f896f9b83244a5548402
timwrb <wurmbrand.tim@gmail.com>
2024-04-18 11:48:05 +0200

    finally got the card fully responsive

--- components/JobCardRP.vue
+++ components/JobCardRP.vue
@@ -4,35 +4,35 @@
   <!-- <NuxtLink :to="'/jobview/' + id"> -->
 <!-- <a :href="'/jobview/' + id"> -->
     <div class="justify-center flex mb-2 cursor-pointer" @click="visible = true">
-      <div class="w-full h-[7.25rem] sm:h-[10rem] pr-2 bg-blur-3xl border border-px rounded-[1rem] transform hover:translate-x-1 transition-transform duration-150 ease-in-out hover:border-customBlue">
+      <div class="w-full sm:h-[10rem] pr-2 bg-blur-3xl border border-px rounded-[1rem] transform hover:translate-x-1 transition-transform duration-150 ease-in-out hover:border-customBlue">
         <div class="flex">
           <div class="hidden sm:flex border border-px border-darkGrey40 rounded-[1.5rem] mt-[1.875rem] ml-[2rem] h-[4rem] w-[4rem] bg-white"></div>
           <div class="flex flex-col ml-[1.75rem] mt-[1.5rem] sm:mt-[2.375rem]">
           <span class="hidden sm:flex text-darkGrey40 font-medium text-[0.7rem] sm:text-[0.825rem]">{{ company }}</span>
 
             <div class="flex flex-row items-center gap-2">
-          <div class="flex sm:hidden border border-px border-darkGrey40 rounded-[0.5rem] h-[1.5rem] w-[1.5rem] bg-white"></div>
+          <div class="flex sm:hidden border border-px border-darkGrey40 rounded-[0.5rem] h-[1.25rem] w-[1.25rem] bg-white"></div>
           <span class="flex sm:hidden text-darkGrey40 font-medium text-[0.7rem] sm:text-[0.825rem]">{{ company }}</span>
             </div>
-            <div class="flex">
-          <h1 class=" w-full truncate text-darkGrey100 font-bold text-[1rem] sm:text-[1.2rem] pr-2">{{ title }}</h1>
+            <div class="flex-row w-full mr-2">
+          <h1 class=" w-full truncate text-darkGrey100 font-bold text-[1rem] sm:text-[1.2rem] pr-2 whitespace-normal">{{ title }}</h1>
           </div>
         </div>
         </div>
         <div class="ml-[1.5rem] sm:ml-[2rem] mt-[0.5rem] sm:mt-[1.25rem]">
-          <div class="flex flex-row gap-[0.75rem]">
+          <div class="flex flex-row gap-[0.75rem] mb-2">
       <!-- date -->
-      <div class="h-[1.3rem] sm:h-[1.7rem] rounded-full border-[0.1rem] border-darkGrey100 bg-white justify-center flex items-center">
+      <div class="h-[1.1rem] sm:h-[1.7rem] rounded-full border-[0.05rem] sm:border-[0.1rem] border-darkGrey100 bg-white justify-center flex items-center">
   <!-- Calendar Icon -->
   <svg class="ml-4 sm:flex hidden" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M4.5 0C4.91421 0 5.25 0.335786 5.25 0.75V2.25H14.25V0.75C14.25 0.335786 14.5858 0 15 0C15.4142 0 15.75 0.335786 15.75 0.75V2.25H16.5C18.1569 2.25 19.5 3.59315 19.5 5.25V16.5C19.5 18.1569 18.1569 19.5 16.5 19.5H3C1.34315 19.5 0 18.1569 0 16.5V5.25C0 3.59315 1.34315 2.25 3 2.25H3.75V0.75C3.75 0.335786 4.08579 0 4.5 0ZM18 9C18 8.17157 17.3284 7.5 16.5 7.5H3C2.17157 7.5 1.5 8.17157 1.5 9V16.5C1.5 17.3284 2.17157 18 3 18H16.5C17.3284 18 18 17.3284 18 16.5V9Z" fill="#252323"/>
   </svg>
-      <span class="ml-4 sm:ml-2 mr-4 font-light text-[0.825rem]">{{ date }}</span>
+      <span class="ml-2 sm:ml-2 mr-2 sm:mr-4 font-light text-[0.65rem] sm:text-[0.825rem]">{{ date }}</span>
       </div>
 
       <!-- city -->
-      <div class="h-[1.3rem] sm:h-[1.7rem] rounded-full border-[0.08rem] border-darkGrey100 bg-white justify-center flex items-center">
-      <span class="ml-3 mr-3 bg-gradientBlue bg-clip-text text-transparent font-medium text-[0.75rem] sm:text-[0.825rem]">{{ city }}</span>
+      <div class="h-[1.1rem] sm:h-[1.7rem] rounded-full border-[0.05rem] sm:border-[0.08rem] border-darkGrey100 bg-white justify-center flex items-center">
+      <span class="ml-3 mr-3 bg-gradientBlue bg-clip-text text-transparent font-medium text-[0.65rem] sm:text-[0.825rem]">{{ city }}</span>
       </div>
 
       <!-- street -->
@@ -41,8 +41,8 @@
       </div>
 
       <!-- salary -->
-      <div class="h-[1.3rem] sm:h-[1.7rem] rounded-full border-[0.08rem] border-darkGrey100 bg-white justify-center flex items-center">
-      <span class="ml-3 mr-3 bg-gradientBlue bg-clip-text text-transparent font-medium text-[0.75rem] sm:text-[0.825rem]">{{ salary }}</span>
+      <div class="h-[1.1rem] sm:h-[1.7rem] rounded-full border-[0.05rem] sm:border-[0.08rem] border-darkGrey100 bg-white justify-center flex items-center">
+      <span class="ml-3 mr-3 bg-gradientBlue bg-clip-text text-transparent font-medium text-[0.65rem] sm:text-[0.825rem]">{{ salary }}</span>
       </div>
 
           </div>
