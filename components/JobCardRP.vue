625f43b9c3235e36586a717ab98d8fce4b761040
timwrb <wurmbrand.tim@gmail.com>
2024-04-29 00:48:49 +0200

    test

--- components/JobCardRP.vue
+++ components/JobCardRP.vue
@@ -75,7 +75,7 @@
       </div>
 
     <div class="mt-6 place-content-between flex">
-      <a :href="'/jobview/' + id" class="btn btn-sm md:btn-md btn-outline btn-link hover:underline hover:bg-almostWhite hover:text-darkGrey100">
+      <a :href="'/jobview/' + id" class="btn btn-sm md:btn-md btn-outline btn-link rounded-full hover:underline hover:bg-almostWhite hover:text-darkGrey100">
         <span class="hidden md:flex">Weiter zur Stellenanzeige</span>
         <span class="flex md:hidden">Zur Anzeige</span> <!-- arrow-up-right heroicons -->
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -83,7 +83,7 @@
         </svg>
       </a>
 
-      <p class="btn btn-sm md:btn-md btn-outline btn-ghost hover:bg-almostWhite hover:text-darkGrey100">
+      <p class="btn btn-sm md:btn-md btn-outline btn-ghost hover:bg-almostWhite hover:text-darkGrey100 rounded-full">
         <p class="hidden sm:flex">Speichern</p> <!-- arrow-up-right heroicons -->
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
